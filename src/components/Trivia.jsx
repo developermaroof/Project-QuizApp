d1fc2a6851800dc3277f071e58837585e8c0e870
Maroof-Ali-Qureshi <123650922+Maroof-Ali-Qureshi@users.noreply.github.com>
2023-07-21 20:53:27 +0500

    Updated

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const Trivia = ({ data, setTimeOut, questionNumber, setQuestionNumber }) => {
+const Trivia = ({ data, setStop, questionNumber, setQuestionNumber }) => {
   const [question, setQuestion] = useState(null);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [className, setClassName] = useState("answer");
@@ -9,9 +9,26 @@ const Trivia = ({ data, setTimeOut, questionNumber, setQuestionNumber }) => {
     setQuestion(data[questionNumber - 1]);
   }, [data, questionNumber]);
 
+  const delay = (duration, callback) => {
+    setTimeout(() => {
+      callback();
+    }, duration);
+  };
+
   const handleClick = (a) => {
     setSelectedAnswer(a);
     setClassName("answer active");
+    delay(3000, () =>
+      setClassName(a.correct ? "answer correct" : "answer wrong")
+    );
+    delay(6000, () => {
+      if (a.correct) {
+        setQuestionNumber((prev) => prev + 1);
+        setSelectedAnswer(null);
+      } else {
+        setStop(true);
+      }
+    });
   };
 
   return (
